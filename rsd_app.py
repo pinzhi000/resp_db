0c21b285c51fb8122658755218eaf7bdc945d834
Pinzhi <pinzhi@umich.edu>
2022-05-10 20:23:53 -0500

    Add files via upload

--- rsd_app.py
+++ rsd_app.py
@@ -156,7 +156,7 @@ if app_mode == 'Real-time Prediction':
             audio_file_reshaped = np.reshape(audio_array, [1, 193, 1, 1])
 
 
-            model = tensorflow.keras.models.load_model(path+'./saved_model')
+            model = tensorflow.keras.models.load_model(path+'/saved_model') #set filepath for Github 
 
             # loading spinner 
             with st.spinner('Calculating...'):
